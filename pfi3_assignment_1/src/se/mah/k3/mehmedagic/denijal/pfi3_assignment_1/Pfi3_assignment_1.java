cebc9b0fad90d345f9108a24f1beb2e3b33fa425
Deno <Deno@10.1.1.103>
2011-04-01 15:03:05 +0200

    Signed-off-by: Deno <Deno@10.1.1.103>

---
+++ pfi3_assignment_1/src/se/mah/k3/mehmedagic/denijal/pfi3_assignment_1/Pfi3_assignment_1.java
@@ -0,0 +1,13 @@
+package se.mah.k3.mehmedagic.denijal.pfi3_assignment_1;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Pfi3_assignment_1 extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
